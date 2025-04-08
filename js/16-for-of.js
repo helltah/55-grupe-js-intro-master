9f9b803109c9d66fe27709de120eeb21189f29bb
Vitalij <sestelinskij21>
2025-04-08 09:36:21 +0300

    for-of

---
+++ js/16-for-of.js
@@ -0,0 +1,29 @@
+const marks = [10, 5, 6, 5, 8];
+let sum = 0
+
+for (let i = 0; i < marks.length; i++) {
+    const mark = marks[i];
+    sum += mark;
+}
+console.log(sum);
+
+
+
+
+let sum2 = 0;
+
+for (const mark of marks) {
+    sum2 += mark;
+}
+console.log(sum2);
+
+
+const names = ['Jonas', 'Maryte', 'Petras', 'Ona'];
+const nameSizes = [];
+
+for (const name of names) {
+    
+    nameSizes.push;
+}
+console.log(nameSizes);
+
